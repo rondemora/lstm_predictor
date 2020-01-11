e975cc11b70235618e20027afca1ae30c5119f6c
agonzalez-Ubuntu <amgonzalez911@gmail.com>
2020-01-12 00:22:01 +0100

    New venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/mnt/code/tfg2019/venv"
+set -gx VIRTUAL_ENV "/mnt/code/tfg2019IA/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
